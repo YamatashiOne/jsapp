83a186e41dace08098456464b8d4f2cecd09d5c1
YamatashiOne <boogieleet@gmail.com>
2023-03-28 08:41:52 +0200

    full size

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели");
-console.log(prompt);
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Какой последний фильм вы смотрели?", ""),
+  b = prompt("На сколько вы его оцените?", ""),
+  c = prompt("Какой последний фильм вы смотрели?", ""),
+  d = prompt("На сколько вы его оцените?", "");
+personalMovieDB.movies[d] = c;
+personalMovieDB.movies[b] = a;
+console.log(personalMovieDB);
